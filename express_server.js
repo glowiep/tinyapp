9b0d958c70b5ee397f264128d07c535ebccbfee9
Gloria Lim <glorialimartt@gmail.com>
2023-10-26 00:30:12 -0400

    Modify code order

--- express_server.js
+++ express_server.js
@@ -17,14 +17,14 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase}
   res.render("urls_index", templateVars);
 })
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
