2b7bf0d9c1bf24be2329cf636b38416ec82eebc2
Gloria Lim <glorialimartt@gmail.com>
2023-10-26 00:50:36 -0400

    Add middleware to parse the POST request body

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,21 +23,21 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase}
+  const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
-})
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id]
-  }
-  res.render("urls_show", templateVars)
-})
+  };
+  res.render("urls_show", templateVars);
+});
 
 
 app.listen(PORT, () => {
