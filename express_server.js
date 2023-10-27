6fccc830375f114e42f02382f8101f3f5c3524c2
Gloria Lim <glorialimartt@gmail.com>
2023-10-27 01:13:35 -0400

    Pass in the username to the EJS templates using cookie-parser to facilitate the process

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,20 +26,26 @@ app.get("/hello", (req, res) => {
 
 // My URLs landing page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
 // Create TinyURL page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 // TinyURL info page
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
