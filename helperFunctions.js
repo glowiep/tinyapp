dd7754a812cf9960d10738931da5d89e259a29b2
Gloria Lim <glorialimartt@gmail.com>
2023-10-28 00:37:17 -0400

    Create an email lookup helper function getUserByEmail()

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,12 +1,22 @@
 // Returns a string of 6 random alphanumeric characters
-function generateRandomString() {
+const generateRandomString = function() {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let count = 0; count < 6; count++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
-}
+};
 
+// Return user object based on email, retun null if not found
+const getUserByEmail = function(email, userDatabase) {
+  for (let user in userDatabase) {
+    if (userDatabase[user].email === email) {
+      console.log(userDatabase[user].id, email);
+      return userDatabase[user];
+    }
+  }
+  return null;
+};
 
-module.exports = { generateRandomString }
+module.exports = { generateRandomString, getUserByEmail };
