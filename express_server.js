cd7abfcf418c475fa22c490a98ccec1c6790adf5
Gloria Lim <glorialimartt@gmail.com>
2023-11-02 23:02:45 -0400

    Refactor POST /urls/:id for a more efficient logic flow.

--- express_server.js
+++ express_server.js
@@ -218,24 +218,24 @@ app.post("/urls/:id", (req, res) => {
   const user = req.session.user_id;
   const urlID = req.params.id;
 
-  // Check if the user has permission to modify the urlID
-  if (checkUrlId(urlID, user, urlDatabase)) {
-    urlDatabase[urlID].longURL = req.body.UpdatedLongURL;
-    return res.redirect("/urls");
+  if (!user) {  // if the user is not logged in
+    return res.status(401).send(`Unauthorized request. Please ${logInLink} to view or modify your TinyURL.\n`);
   }
   
-  // When user is logged in but does not own the url, and the urlID exists in the database
-  if (users[user] && !checkUrlId(urlID, user, urlDatabase) && checkUrlIdExists(urlID, urlDatabase)) {
-    return res.status(401).send("Unauthorized request. This user does not own the TinyURL.\n");
-  }
-  
-  // When the urlID does not exists in the entire database
+  // Check if urlID exists in the entire database
   if (!checkUrlIdExists(urlID, urlDatabase)) {
     return res.status(404).send("This TinyURL does not exist.\n");
   }
+
+  // Check if the user has permission to modify the urlID
+  if (!checkUrlId(urlID, user, urlDatabase)) {
+    return res.status(401).send("Unauthorized request. This user does not own the TinyURL.\n");
+  }
   
-  // Users that are not logged in are not authorized
-  return res.status(401).send(`Unauthorized request. Please ${logInLink} to view or modify your TinyURL.\n`);
+  // Happy path - User is logged in, owns the url, and the urlID exists in the database
+  urlDatabase[urlID].longURL = req.body.UpdatedLongURL;
+  return res.redirect("/urls");
+  
 });
 
 
