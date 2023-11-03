4b94f7bce24da7ec1fda70382d3ad232f0cafc80
Gloria Lim <glorialimartt@gmail.com>
2023-11-02 23:40:00 -0400

    Add unit test for getUserByEmail function

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,21 +4,26 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.strictEqual(user.id, expectedUserID);
+  });
+
+  it('should return null when provided an email that does not exist in the database', function() {
+    const user = getUserByEmail("user123@gmail.com", testUsers);
+    assert.isNull(user);
   });
 });
